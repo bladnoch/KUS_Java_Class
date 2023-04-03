f3367b01f97d843ed91f6d94b5da3fff9c5b944a
DK <dongwook5478@gmail.com>
2023-04-03 17:17:21 +0900

    4/3/2023(6-7,6-8,7-1)

--- src/chapter7/Bicycle.java
+++ src/chapter7/Bicycle.java
@@ -3,8 +3,26 @@ package chapter7;
 public class Bicycle {
     //Data member
     private String ownerName;
-    public void Bicycle(){
-        ownerName="Unknown";
+    private String brand;
+    private int price;
+
+    public String getBrand(){
+        return brand;
+    }
+    public void setBrand(String name){
+        brand=name;
+    }
+    public Bicycle(){
+        ownerName="OEM";
+        brand="3000";
+        price=10000;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+    public void setPrice(int p){
+        price=p;
     }
     public String getOwnerName(){
         return ownerName;
--- src/chapter7/Chap7Program1.java
+++ src/chapter7/Chap7Program1.java
@@ -13,14 +13,24 @@ public class Chap7Program1 {
 
         bike1=new Bicycle();
         bike1.setOwnerName("Minseok Seo");
+        bike1.setBrand("BMC");
+        bike1.setPrice(100000000);
+        System.out.println(bike1.getBrand());
+
 
         bike2=new Bicycle();
         bike2.setOwnerName("IU");
+        bike2.setBrand("TREK");
+        System.out.println(bike2.getBrand());
 
         owner1=bike1.getOwnerName();
         owner2=bike2.getOwnerName();
 
-        System.out.println(owner1+" owns a bicycle.");
+        System.out.println(bike1.getOwnerName()+" owns a bicycle.");
         System.out.println(owner2+" also owns a bicycle.");
+
+        System.out.println(bike1.getPrice()+" owns a bicycle.");
+        System.out.println(bike2.getPrice()+" also owns a bicycle.");
+
     }
 }

96221ac9da3ace2fab3425be0ac0d94f811f243b
DK <dongwook5478@gmail.com>
2023-04-11 16:07:46 +0900

    update Q3

--- src/Homework_2/Q3.java
+++ src/Homework_2/Q3.java
@@ -1,22 +1,29 @@
 package Homework_2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Q3.java
- *
+ * Get formula and show calc. step by step
  * @since 2023-4-11
  * @author Dounguk kim
  */
 public class Q3 {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         System.out.println("entered formula");
         String formula=sc.nextLine();
-        System.out.println(formula);
+        String[] form=formula.split(" ");
 
-
-
-
+        for (int i=0;i<form.length;i++){
+            System.out.println(form[i]);
+        }
+        if (form[1].equals("+")){
+            System.out.println(Integer.parseInt(form[0])+ Integer.parseInt(form[2]));
+        }
+        else{
+            System.out.println("nope");
+        }
     }
 }

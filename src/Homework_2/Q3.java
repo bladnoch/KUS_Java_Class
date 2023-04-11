ff4c75b7ed498ef756cb8725b3db503fe86c8a49
DK <dongwook5478@gmail.com>
2023-04-11 20:41:46 +0900

    finished Q3.java

--- src/Homework_2/Q3.java
+++ src/Homework_2/Q3.java
@@ -1,6 +1,5 @@
 package Homework_2;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -10,20 +9,51 @@ import java.util.Scanner;
  * @author Dounguk kim
  */
 public class Q3 {
+    public static int calc(int a,String and, int b){
+        int afterCalc=0;
+        if(and.equals("+")){
+            afterCalc=a+b;
+        }
+        else if(and.equals("-")){
+            afterCalc=a-b;
+        }
+        else if(and.equals("/")){
+            afterCalc=a/b;
+        }
+        else if(and.equals("*")){
+            afterCalc=a*b;
+        }
+        return afterCalc;
+    }
+
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
-        System.out.println("entered formula");
-        String formula=sc.nextLine();
-        String[] form=formula.split(" ");
 
-        for (int i=0;i<form.length;i++){
-            System.out.println(form[i]);
+
+        System.out.print("Java Q3.java ");
+        String formula=sc.nextLine(); //식 입력
+        System.out.print("entered formula: ");
+        System.out.println(formula+"\n");
+
+
+        String[] form=formula.split(" "); //식 나눠서 저장
+        int allCalc=0;
+        int count=2;
+
+        allCalc=calc(Integer.parseInt(form[0]),form[1],Integer.parseInt(form[2]));
+        System.out.println("Operation1: "+form[0]+" "+form[1]+" "+form[2]+" = "+allCalc);
+
+        for(int i=3;i<form.length;i+=2){
+
+            System.out.print("Operation"+count+": "+allCalc+" "+form[i]+" "+form[i+1]+" = ");
+            allCalc=calc(allCalc,form[i],Integer.parseInt(form[i+1]));
+            System.out.println(allCalc);
+            count++;
+
         }
-        if (form[1].equals("+")){
-            System.out.println(Integer.parseInt(form[0])+ Integer.parseInt(form[2]));
-        }
-        else{
-            System.out.println("nope");
-        }
+
+        System.out.println("The result of the entered formula is "+count+".");
+        System.out.println("The actual result of the compiler operation in Java is "+(count-1)+".");
+
     }
 }

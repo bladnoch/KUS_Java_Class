af4a0b78862c74ffc102aadfe98b52e810c04ec0
DK <dongwook5478@gmail.com>
2023-04-12 12:48:06 +0900

    update comments Q6

--- src/Homework_2/Q6.java
+++ src/Homework_2/Q6.java
@@ -5,6 +5,11 @@ package Homework_2;
  * @since 2023-4-12
  * @author Dounguk kim
  */
+
+//1000개의 은행계좌를 생성하고, 계좌주는 주인_1, 주인_2..., 은행 잔고의 초가값은 0-50000 사이 랜덤하게 할당
+//모든 은행계좌에 10000추가
+//잔고가 가장 큰 계좌의 이름과 잔를 출력
+
 public class Q6 {
     public static void main(String[] args){
         BankAccount a=new BankAccount();

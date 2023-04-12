ff20c0969476ffc9f45281725663cee645dd6ba7
DK <dongwook5478@gmail.com>
2023-04-12 12:21:25 +0900

    finished Q5

--- src/Homework_2/Q5.java
+++ src/Homework_2/Q5.java
@@ -1,6 +1,5 @@
 package Homework_2;
 
-import javax.swing.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,6 +10,8 @@ import java.util.Scanner;
  * @author Dounguk kim
  */
 public class Q5 {
+    final static double WFEMALE=37.8;
+    final static double WMALE=40;
     public static int setGender(){
         Scanner sc=new Scanner(System.in);
         int gen=0;
@@ -28,23 +29,29 @@ public class Q5 {
         }
         return gen;
     }
-    public static void mFm(int gen){
+    public static void condition(int gen, int height){
         if (gen==1){
-            idealMale();
+            idealMale(height);
+        }
+        else{
+            idealFemale(height);
+        }
+    }
+    public static void idealMale(int h){
+        if(h<150){
+            System.out.println("Your ideal weight is "+WMALE+"kg");
         }
         else {
-            idealFemale();
+            System.out.println("Your ideal weight is "+(WMALE+0.89*(h-150))+"kg");
         }
     }
-    public static double idealMale(){
-
-
-
-
-    }
-    public static double idealFemale(){
-
-
+    public static void idealFemale(int h){
+        if(h<150){
+            System.out.println("Your ideal weight is "+WFEMALE+"kg");
+        }
+        else {
+            System.out.println("Your ideal weight is "+(WFEMALE+0.89*(h-150))+"kg");
+        }
 
     }
     public static int setHeight(){
@@ -56,23 +63,16 @@ public class Q5 {
                 height=sc.nextInt();
             } catch (InputMismatchException e){
                 sc.nextLine();
+                System.out.println("How tall are you?(cm)");
             }
-            System.out.println("How tall are you?(cm)");
         }
         return height;
     }
-
     public static void main(String[] args){
         int gen=setGender();
         int height=setHeight();
+        condition(gen,height);
 
-        System.out.println(height);
-        if(gen==1){
-            System.out.println("male");
-        }
-        else if(gen==2){
-            System.out.println("female");
-        }
 
     }
 }

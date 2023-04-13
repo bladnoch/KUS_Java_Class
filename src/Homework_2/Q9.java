45017f92e722e30876e76d080081e5b1be292699
DK <dongwook5478@gmail.com>
2023-04-13 12:40:22 +0900

    add Q9

---
+++ src/Homework_2/Q9.java
@@ -0,0 +1,43 @@
+package Homework_2;
+
+import java.util.Scanner;
+
+
+/**
+ * Q8.java
+ * make translater that translate English to Korean
+ * @since 2023-4-13
+ * @author Dounguk kim
+ */
+
+public class Q9 {
+
+    public static void getSum(int value,int value2 ){               //정수 2개
+
+    }
+    public static void getSum(int value1,int value2,int valuee3 ){  //정수 3개
+
+    }
+    public static void getSum(int[] value){                         //정수 여러개
+
+    }
+    public static void getSum(double[] value){                      //실수 여러개
+
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[] iInput;
+        double[] dInput;
+        String[] sInput;
+
+        //input ui
+
+
+
+        //calc
+
+
+        //output
+    }
+}

1a828f19bf4f15210ad5377e05f5bcd0dfff11b1
DK <dongwook5478@gmail.com>
2023-04-13 10:28:13 +0900

    finished Q7

--- src/Homework_2/Q7.java
+++ src/Homework_2/Q7.java
@@ -1,14 +1,64 @@
 package Homework_2;
+
+import java.util.Scanner;
+import static java.lang.Math.random;
+
 /**
  * Q6.java
- * To creat BankAccount class and make a part of banking system
+ * Play rock, scissors, and paper game with computer
  * @since 2023-4-12
  * @author Dounguk kim
  */
 public class Q7 {
+    public static void winner(String[] value){
+        Scanner sc=new Scanner(System.in);
+        String me;
+        int comp=(int)(random()*3);
+
+        comp=(int)(random()*3);
+        System.out.print("GAME START\n(rock, scissors, paper)\nPlayer: ");
+        me=sc.next();
+        if(me.equals(value[0]) ){ //rock
+            if(comp==0){
+                System.out.println("computer: paper\n You win!");
+            } else if (comp==1) {
+                System.out.println("computer: scissors\n Draw!");
+            }
+            else{
+                System.out.println("computer: rock\n You lose!");
+            }
+        }
+        else if(me.equals(value[1]) ){ //scissors
+            if(comp==0){
+                System.out.println("computer: rock\n Draw!");
+            } else if (comp==1) {
+                System.out.println("computer: paper\n You lose!");
+            }
+            else{
+                System.out.println("computer: scissors\n You win!");
+            }
+        }
+        else if(me.equals(value[2]) ){ //paper
+            if(comp==0){
+                System.out.println("computer: scissors\n You lose!");
+            } else if (comp==1) {
+                System.out.println("computer: rock\n You win!");
+            }
+            else{
+                System.out.println("computer: paper\n Draw!");
+            }
+        }
+        else{
+            System.out.println("GAME OVER..");
+        }
+        System.out.println("");
+
+    }
     public static void main(String[] args){
         String str[] = {"scissors", "rock", "paper"};
-        int n = (int)(Math.random()*3); // it will generates 1~3 value randomly.
+        int n = (int)(random()*3); // it will generates 1~3 value randomly.
+        winner(str);
+
 
     }
 }

67961aab78d5c7640cb1c8adb95861833705ff6b
DK <dongwook5478@gmail.com>
2023-04-16 00:24:28 +0900

    update Q7

--- src/Homework_2/Q5.java
+++ src/Homework_2/Q5.java
@@ -12,6 +12,12 @@ import java.util.Scanner;
 public class Q5 {
     final static double WFEMALE=37.8;
     final static double WMALE=40;
+
+    /**
+     * get input for set gender
+     * 성별을 정할 인풋을 받는다.
+     * @return gen
+     */
     public static int setGender(){
         Scanner sc=new Scanner(System.in);
         int gen=0;
@@ -29,6 +35,13 @@ public class Q5 {
         }
         return gen;
     }
+
+    /**
+     * after get gender by setGender start calculation methods by gender
+     * setGender에서 값을 받은 후 각 성별에 맞는 메소드가 실행된다.
+     * @param gen
+     * @param height
+     */
     public static void condition(int gen, int height){
         if (gen==1){
             idealMale(height);
@@ -37,6 +50,12 @@ public class Q5 {
             idealFemale(height);
         }
     }
+
+    /**
+     * calculate male's ideal weight
+     * 남자의 적정 체중을 계산한다.
+     * @param h
+     */
     public static void idealMale(int h){
         if(h<150){
             System.out.println("Your ideal weight is "+WMALE+"kg");
@@ -45,6 +64,12 @@ public class Q5 {
             System.out.println("Your ideal weight is "+(WMALE+0.89*(h-150))+"kg");
         }
     }
+
+    /**
+     * calculate female's ideal weight
+     * 여자의 적정 체중을 계산한다.
+     * @param h
+     */
     public static void idealFemale(int h){
         if(h<150){
             System.out.println("Your ideal weight is "+WFEMALE+"kg");
@@ -54,6 +79,12 @@ public class Q5 {
         }
 
     }
+
+    /**
+     * set height and return height. It has exception check
+     * 높이를 받고 받은 높이를 리턴. 예외처리가 있다
+     * @return
+     */
     public static int setHeight(){
         Scanner sc=new Scanner(System.in);
         int height=0;
--- src/Homework_2/Q6.java
+++ src/Homework_2/Q6.java
@@ -15,10 +15,15 @@ public class Q6 {
     public static void main(String[] args){
         BankAccount richGuy=new BankAccount("부자");
         BankAccount[] owner =new BankAccount[POPULATION];
-
         setPeople(owner);
         highestBalance(richGuy, owner);
     }
+
+    /**
+     * set 1000 people's name, random balance under 50000, and add 10000 on all account
+     * 1000개의 사람의 이름, 랜덤 50000이하의 계좌 금액을 만들고 10000씩 모든 계좌에 추가한다.
+     * @param owner
+     */
     public static void setPeople(BankAccount[] owner){
         for (int i = 0;i<POPULATION;i++){
             owner[i]=new BankAccount("주인_"+(i+1));
@@ -27,6 +32,13 @@ public class Q6 {
         }
 
     }
+
+    /**
+     * set info of highest balance and print it
+     * 계좌 금액이 가장 높은사람을 찾고 프린트 한다.
+     * @param richGuy
+     * @param owner
+     */
     public static void highestBalance(BankAccount richGuy,BankAccount[] owner){
         for (int i = 0;i<POPULATION;i++) {
             if (richGuy.getBalance() < owner[i].getBalance()) {

3f9de453dc5807e299e4a751fc61e4d09af20ed8
DK <dongwook5478@gmail.com>
2023-04-16 00:11:41 +0900

    update Q5

--- src/Homework_2/Q4.java
+++ src/Homework_2/Q4.java
@@ -12,9 +12,21 @@ import java.util.Scanner;
 public class Q4 {
 
     final static int ARRLEN=8;
+
+    /**
+     * start bubble sort with array arr, and number of values-1
+     * @param arr
+     */
     public static void bubbleSort(int[] arr){
         bubbleSort(arr, ARRLEN-1);
     }
+
+    /**
+     * swap the values for sort
+     * 정렬을 위해 값들을 스왑한다.
+     * @param arr
+     * @param lastArr
+     */
     public static void bubbleSort(int[] arr, int lastArr){
         int temp=0;
         if (lastArr>0){
@@ -25,15 +37,27 @@ public class Q4 {
                     arr[i]=temp;
                 }
             }
-            bubbleSort(arr,lastArr-1);
+            bubbleSort(arr,lastArr-1); //lastArr이 하나씩 줄면서 나중엔 루프가 종료
         }
 
     }
+
+    /**
+     * print sorted array
+     * 솔트된 배열을 프린트한다.
+     * @param arr
+     */
     public static void printArr(int[] arr){
         for (int i=0;i<ARRLEN;i++){
             System.out.print(arr[i]+" ");
         }
     }
+
+    /**
+     * get 8 integers and return not sorted array
+     * 정수 8개를 입력받고 입력받은 정리안된 배열을 리턴한다.
+     * @return arr
+     */
     public static int[] makeArr(){
         int[] arr=new int[ARRLEN];
         Scanner sc=new Scanner(System.in);

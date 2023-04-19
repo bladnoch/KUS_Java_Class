7632087a8c9f3b827c65cc4b4b9d3e2de07afd38
DK <dongwook5478@gmail.com>
2023-04-19 13:17:30 +0900

    add ch10-5,6

---
+++ src/chapter10/Chap10Program5.java
@@ -0,0 +1,26 @@
+package chapter10;
+
+import java.io.*;
+
+/**
+ * Chap10Program5.java
+ * Example of Text_file output
+ * @since 4-19-2023
+ * @author Dounguk Kim
+ */
+public class Chap10Program5 {
+    public static void main(String[] args) throws IOException{
+        File outFile = new File("/Users/doungukkim/Desktop/workspace/Java_Inclass/src/chapter10/example2.data");
+        FileOutputStream outStream=new FileOutputStream(outFile);
+        PrintWriter outPW=new PrintWriter(outStream); //사람이 읽을 수 있게 한다.
+
+
+        //readable
+        outPW.println(10);
+        outPW.println("hello world");
+        outPW.println(true);
+
+        outPW.close();
+    }
+}
+
---
+++ src/chapter10/Chap10Program6.java
@@ -0,0 +1,9 @@
+package chapter10;
+/**
+ * Chap10Program6.java
+ *
+ * @since 4-19-2023
+ * @author Dounguk Kim
+ */
+public class Chap10Program6 {
+}

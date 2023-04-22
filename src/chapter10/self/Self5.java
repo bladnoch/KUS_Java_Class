fb1bcda4e257fc9f30077bde17cd8fa1032ac6ea
DK <dongwook5478@gmail.com>
2023-04-22 23:08:18 +0900

    add self.data

--- src/chapter10/self/Self5.java
+++ src/chapter10/self/Self5.java
@@ -1,5 +1,10 @@
 package chapter10.self;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Self5.java
  * study ch10program5
@@ -7,7 +12,15 @@ package chapter10.self;
  * @author Dounguk Kim
  */
 public class Self5 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        File outFile=new File("/Users/doungukkim/Desktop/workspace/Java_Inclass/src/chapter10/self/self5.date");
+        FileOutputStream outStream=new FileOutputStream(outFile);
+        PrintWriter outWrite=new PrintWriter(outStream);
+
+        outWrite.println("this is Daniel Kim");
+        outWrite.println(true);
+
+        outWrite.close();
 
     }
 }

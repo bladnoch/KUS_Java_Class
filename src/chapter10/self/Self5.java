2429282ede22c97d15c2d0001073877b01e8f206
DK <dongwook5478@gmail.com>
2023-04-22 22:58:52 +0900

    add Self5

---
+++ src/chapter10/self/Self5.java
@@ -0,0 +1,13 @@
+package chapter10.self;
+
+/**
+ * Self5.java
+ * study ch10program5
+ * @since 4-22-2023
+ * @author Dounguk Kim
+ */
+public class Self5 {
+    public static void main(String[] args){
+
+    }
+}

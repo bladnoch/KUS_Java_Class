fe5a80e934c41819420cd9aaf61112566df22136
DK <dongwook5478@gmail.com>
2023-04-23 15:30:13 +0900

    update TQ3 58min

--- src/Homework_2/selfHW/TQ2.java
+++ src/Homework_2/selfHW/TQ2.java
@@ -1,36 +1,48 @@
 package Homework_2.selfHW;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * TQ2.java
- * get 10 ints get average, max value
- * @since 4-24-2023
+ * get numbers and print max, and average value
+ * @since 4/23/2023
  * @author Dounguk Kim
  */
 public class TQ2 {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int nums=0;
-        int num=0;
-        int max=0;
-        int count=0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[10]; // 최대 10개의 숫자를 저장할 수 있는 배열
+        int count = 0; // 입력된 숫자의 개수
+        int sum = 0; // 숫자의 합
 
-        for (int i=0;i<10;i++){
-            System.out.print((i+1)+"th value >>");
-            try{
-                num=sc.nextInt();
-            } catch (InputMismatchException e){
+        while (count < 10) {
+            try {
+                int number = scanner.nextInt();
+                numbers[count] = number;
+                count++;
+                sum += number;
+            } catch (InputMismatchException e) {
+                System.out.println("숫자가 아닙니다. 입력을 종료합니다.");
                 break;
             }
-            nums+=num;
-            count++;
-            if(max<num){
-                max=num;
-            }
         }
-        System.out.println("Maximum value: "+max);
-        System.out.println("Average value: "+nums/count);
+
+        if (count == 0) {
+            System.out.println("입력된 숫자가 없습니다.");
+        } else {
+            int max = numbers[0];
+            for (int i = 1; i < count; i++) {
+                if (numbers[i] > max) {
+                    max = numbers[i];
+                }
+            }
+            double average = (double) sum / count;
+            System.out.println("평균: " + average);
+            System.out.println("최대값: " + max);
+        }
     }
+
 }
--- src/Homework_2/selfHW/TQ3.java
+++ src/Homework_2/selfHW/TQ3.java
@@ -1,4 +1,59 @@
 package Homework_2.selfHW;
 
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
 public class TQ3 {
+    public static void numOrNot(String[] splited){
+        ArrayList<Integer> num= new ArrayList<>();
+        ArrayList<String> eq=new ArrayList<>();
+
+        int count=1;
+        int temp=0;
+        for (int i=0;i<splited.length;i++){
+            if (i%2==0){
+                num.add(Integer.parseInt(splited[i]));
+            }
+            else {
+                eq.add(splited[i]);
+            }
+        }
+        temp=0;
+        for (int i=0;i<splited.length;i++) {
+            try {
+                if (eq.get(i).equals("+")) {
+                    System.out.println("Opertation" + (count) + ": " + num.get(i) + " + " + num.get(i+1) + " = " + (num.get(i) +num.get(i+1)));
+                    num.set(i+1,(num.get(i) +num.get(i+1)));
+                    count++;
+                } else if (eq.get(i).equals("-")) {
+                    System.out.println("Opertation" + (count) + ": " + num.get(i) + " - " + num.get(i+1) + " = " + (num.get(i) -num.get(i+1)));
+                    num.set(i+1,(num.get(i) -num.get(i+1)));
+                    count++;
+                } else if (eq.get(i).equals("/")) {
+                    System.out.println("Opertation" + (count) + ": " + num.get(i) + " / " + num.get(i+1) + " = " + (num.get(i) /num.get(i+1)));
+                    num.set(i+1,(num.get(i) /num.get(i+1)));
+                    count++;
+                } else if (eq.get(i).equals("*")) {
+                    System.out.println("Opertation" + (count) + ": " + num.get(i) + " * " + num.get(i+1) + " = " + (num.get(i) *num.get(i+1)));
+                    num.set(i+1,(num.get(i) *num.get(i+1)));
+                    count++;
+                }
+            } catch (IndexOutOfBoundsException e){
+//                System.out.println(" end ");
+                break;
+            }
+        }
+        System.out.println("result of formula is "+(count));
+        System.out.println("The actual result of the compiler operation in JAVA is "+(count-1));
+
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String formula=sc.nextLine();
+        System.out.println("Entered formula: "+formula);
+        String[] splitedArray=formula.split(" ");
+        numOrNot(splitedArray);
+
+    }
 }

9859f890294a0eecc65ab704cfaf1e3cbc91cbb1
DK <dongwook5478@gmail.com>
2023-04-23 18:30:52 +0900

    update TQ7

--- src/Homework_2/selfHW/TQ7.java
+++ src/Homework_2/selfHW/TQ7.java
@@ -1,4 +1,62 @@
 package Homework_2.selfHW;
 
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * TQ7.java
+ * make rock scissors and paper game
+ * @since 4/23/2023
+ * @author Dounguk Kim
+ */
 public class TQ7 {
+    public static void calc(int playerNum,int computer){
+        if (playerNum==computer){
+            System.out.println("draw");
+        } else if (playerNum==0 && computer==1) {
+            System.out.println("you lose");
+        } else if(playerNum==0 && computer==2){
+            System.out.println("you win");
+        }
+        else if (playerNum>computer){
+            System.out.println("you win");
+        }
+        else {
+            System.out.println("you lose");
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Random rd=new Random();
+
+        String[] str = {"scissors", "rock", "paper"};
+        int n = (int)(Math.random()*3);
+        String player;
+        int playerNum;
+//        try{
+            player=sc.next();
+//        } catch(){
+//            break;
+//        }
+
+        System.out.println("Computer: "+str[n]);
+
+        if (player.equals(str[0]) || player.equals(str[1]) || player.equals(str[2])){
+            if (player.equals(str[0])){ //플레이어 sciss
+                playerNum=0;
+                calc(0,n);
+            } else if (player.equals(str[1])) { //rock
+                playerNum=1;
+                calc(1,n);
+            }else { //paper
+                playerNum=2;
+                calc(2,n);
+            }
+        }
+
+
+
+
+
+    }
 }

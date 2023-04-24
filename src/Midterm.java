8ffe3b114c0b683e7a4c092e0988cb83ab6b465b
DK <dongwook5478@gmail.com>
2023-04-24 14:15:12 +0900

    add Midterm.java for final

---
+++ src/Midterm.java
@@ -0,0 +1,44 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *  .java
+ *
+ * @since 4-24-2023
+ * @author Dounguk Kim
+ */
+public class Midterm{
+    public static void main(String[] args){
+
+    }
+}
+
+// 오버라이드 사용 Q9
+
+// 랜덤 값 만들기 Q1
+
+// 정수 아닐 때 예외처리 Q2
+//   for( int i =0;i<INPUT;i++){
+//        System.out.print((i+1)+"th value >> ");
+//        try {
+//        num[i]=sc.nextInt();
+//        } catch (InputMismatchException e){
+//        System.out.println("Input value is not integer. Please try it again!");
+//        sc.nextLine();
+//        i--;
+//        }
+//        }`
+
+// 랜덤 숫자 생성
+
+// bubble sort Q4
+
+// 랜덤값 생성 Q6
+//  owner[i].setInitialBalance((int)(Math.random()*50000));
+
+// Map Q8
+//  Map dictionary=new TreeMap();
+//  dic.put("effect","영향");
+//  dic.keySet()
+//  dic.containsKey(word[i])
+//  dic.get(word[i])

4526151e9c60d717ee93fa45a9eb22e5ae89e7ed
DK <dongwook5478@gmail.com>
2023-04-24 13:44:54 +0900

    T9 46m

--- src/Homework_2/selfHW/Stud.java
+++ src/Homework_2/selfHW/Stud.java
@@ -1,4 +1,59 @@
 package Homework_2.selfHW;
 
 public class Stud {
+    /**
+     * 정수 2개짜리
+     * @param firstNum 정수1
+     * @param secondNum 정수2
+     * @return 더한 값
+     */
+    public int getSums(int firstNum, int secondNum) {
+
+        return firstNum+secondNum;
+    }
+
+}
+class Stud2 extends Stud {
+    /**
+     * 3개짜리
+     * @param firstNum 정수1
+     * @param secondNum 정수2
+     * @param thirdNum 정수3
+     * @return 더한 값
+     */
+    public int getSums(int firstNum, int secondNum, int thirdNum) {
+
+        return firstNum+secondNum+thirdNum;
+    }
+
+}
+
+class Stud3 extends Stud2 {
+    /**
+     *  정수 배열 받는 메소드
+     * @param nums 모든 정수 배열
+     * @return 모든 정수 더한 값
+     */
+    public int getSums(int[] nums) {
+        int temp=0;
+        for (int i=0; i<nums.length;i++){
+            temp+=nums[i];
+        }
+        return temp;
+    }
+
+}
+class Stud4 extends Stud3 {
+    /**
+     * 모든 실부 받는 곳
+     * @param nums 모든 실수
+     * @return 모든 실수 더한 값
+     */
+    public double getSums(double[] nums){
+        double temp=0;
+        for (int i=0; i<nums.length;i++){
+            temp+=nums[i];
+        }
+        return temp;
+    }
 }
--- src/Homework_2/selfHW/TQ9.java
+++ src/Homework_2/selfHW/TQ9.java
@@ -1,4 +1,102 @@
 package Homework_2.selfHW;
 
-public class TQ9 {
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *  TQ9.java
+ *  make solution with overriding
+ * @since 4-24-2023
+ * @author Dounguk Kim
+ */
+public class TQ9{
+
+    public static void main(String[] args){
+        ArrayList<Integer> intValue=new ArrayList<>();
+        ArrayList<Double> doubleValue=new ArrayList<>();
+        Stud4 sum= new Stud4();
+        int intCount=0;
+        int doubleCount=0;
+
+        Scanner sc=new Scanner(System.in);
+
+        /**
+         * 모든 값들을 받는다 정수 실수 아닐경우 예외처리
+         */
+        while (true){
+            System.out.println("put number(quit) ");
+            String temp=sc.next();
+            if(temp.equals("quit")){
+                break;
+            }
+            try{
+                intValue.add(Integer.parseInt(temp));
+                intCount+=1;
+            } catch (NumberFormatException e){
+                try{
+                    doubleValue.add(Double.parseDouble (temp));
+                    doubleCount+=1;
+                } catch (NumberFormatException a){
+                    System.out.println("input is not a int or double value");
+                    intCount--;
+                    doubleCount--;
+                }
+            }
+
+        }
+        int[] allNum;
+        double[] allDouble;
+        int count=0;
+
+        /**
+         * int 2개만 있을경우 실행 Stud에 있는 파라미터 2개의 getSum()을 호출
+         */
+        if(doubleValue.size()==0 && intCount==2){
+            allNum=new int[intCount];
+            for (int i=0;i<allNum.length;i++){
+                allNum[i]=intValue.get(i);
+            }
+            System.out.println("sum of all input number is "+sum.getSums(allNum[0],allNum[1]));
+        }
+        /**
+         * int 2개만 있을경우 실행 Stud에 있는 파라미터 2개의 getSum()을 호출
+        */
+        else if(doubleValue.size()==0 && intCount==3){
+            allNum=new int[intCount];
+            for (int i=0;i<allNum.length;i++){
+                allNum[i]=intValue.get(i);
+            }
+            System.out.println("sum of all input number is "+sum.getSums(allNum[0],allNum[1],allNum[2]));
+        }
+        /**
+         * int가 여러개있을경우 실행 Stud에 있는 int array 파라미터의 getSum()을 호출
+         */
+
+        else if(doubleValue.size()==0 && intCount>=4){
+            allNum=new int[intCount];
+            for (int i=0;i<allNum.length;i++){
+                allNum[i]=intValue.get(i);
+            }
+            System.out.println("sum of all input number is "+sum.getSums(allNum));
+        }
+        /**
+         * double이 여러개있을경우 실행 Stud에 있는 double array 파라미터의 getSum()을 호출
+         */
+        else if (doubleValue.size()>=1){
+            allDouble=new double[intCount+doubleCount];
+            for (int i=0;i<doubleValue.size();i++){
+                allDouble[i]=doubleValue.get(i);
+            }
+            for (int i=doubleValue.size();i<doubleValue.size()+ intValue.size();i++){
+                allDouble[i]=intValue.get(count);
+                count++;
+            }
+
+            System.out.println("sum of all input number is "+sum.getSums(allDouble));
+        }
+
+
+
+    }
 }

276d23f58c07621bd28ec366f29e1d0c41649516
DK <dongwook5478@gmail.com>
2023-05-03 15:01:33 +0900

    delete self study files

--- src/chapter11/Self11/P2Self.java
+++
@@ -1,8 +0,0 @@
-package chapter11.Self11;
-
-public class P2Self {
-    public static void main(String[] args){
-        PntColor pc=new PntColor(5,10, "purple");
-        pc.showPointColor();
-    }
-}

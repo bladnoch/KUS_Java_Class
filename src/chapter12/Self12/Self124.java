2c31b815bdce9508cabc2e35029daf6c9b5c5560
DK <dongwook5478@gmail.com>
2023-05-15 14:23:15 +0900

    add Self124

---
+++ src/chapter12/Self12/Self124.java
@@ -0,0 +1,16 @@
+package chapter12.Self12;
+
+import javax.swing.*;
+
+public class Self124 extends JFrame {
+    public Self124(){
+        setTitle("version 4");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        setSize(400,600);
+        setVisible(true);
+    }
+}
+
+//class

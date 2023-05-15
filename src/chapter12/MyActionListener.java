233d9dc977db7247ff1a91fd09e6e38c9d2ea100
DK <dongwook5478@gmail.com>
2023-05-15 16:57:45 +0900

    update 12-3

--- src/chapter12/MyActionListener.java
+++ src/chapter12/MyActionListener.java
@@ -10,10 +10,6 @@ public class MyActionListener implements ActionListener {
         if (b.getText().equals("ON")){
             System.out.println("if");
             b.setText("OFF");
-        } else if (b.getText().equals("Hey")) {
-            b.setText("Hello");
-        } else if (b.getText().equals("Hello")) {
-            b.setText("Hey");
         }
         else {
             b.setText("ON");

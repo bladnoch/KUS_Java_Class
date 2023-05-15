52b976b5ce27b74211db78fbc059c0e088c154b0
DK <dongwook5478@gmail.com>
2023-05-15 12:27:40 +0900

    complete self121, add self122

---
+++ src/chapter12/Self12/Self121.java
@@ -0,0 +1,23 @@
+package chapter12.Self12;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Self121 : Practice 12-1 problem
+ * @version v0.0.1
+ * @author Dounguk Kim
+ * @since 5-12-2023
+ */
+
+public class Self121 extends JFrame {
+
+    Self121(){
+        this.setTitle("JFrame self check problem 12-1");
+        this.setSize(300,300);
+        this.setVisible(true);
+    }
+    public static void main(String[] args){
+        Self121 c=new Self121();
+    }
+}
---
+++ src/chapter12/Self12/Self122.java
@@ -0,0 +1,18 @@
+package chapter12.Self12;
+
+import javax.swing.*;
+/**
+ * Self122 : Practice 12-2 problem
+ * @version v0.0.1
+ * @author Dounguk Kim
+ * @since 5-12-2023
+ */
+
+public class Self122 extends JFrame {
+    Self122(){
+        this.setTitle("version 2");
+    }
+    public static void main(String[] args){
+
+    }
+}

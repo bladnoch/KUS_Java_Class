4993c67981de456498aa93bf8694c4d2165b5f33
DK <dongwook5478@gmail.com>
2023-05-17 13:04:06 +0900

    add 13-1

--- src/chapter13/Program1/Chapter13Program1.java
+++ src/chapter13/Program1/Chapter13Program1.java
@@ -1,4 +1,26 @@
 package chapter13.Program1;
 
-public class Chapter13Program1 {
+import javax.swing.*;
+import java.awt.*;
+
+public class Chapter13Program1 extends JFrame {
+    public Chapter13Program1(){
+        this.setTitle("13-1");
+        Container c=getContentPane();
+        c.setLayout((new FlowLayout()));
+        
+        JLabel timerLabel=new JLabel();
+        timerLabel.setFont(new Font("Gothic",Font.ITALIC, 80));
+        c.add(timerLabel);
+        
+        TimerThread th=new TimerThread(timerLabel);
+        
+        setSize(300,170);
+        setVisible(true);
+        
+        th.start();
+    }
+    public static void main(String[] args){
+        new Chapter13Program1();
+    }
 }

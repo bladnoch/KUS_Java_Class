7c52a70891a2425b014056e2da801a576f9be806
DK <dongwook5478@gmail.com>
2023-05-21 21:33:35 +0900

    update self 12-4

--- src/chapter12/Self4/Chapter12Self4.java
+++ src/chapter12/Self4/Chapter12Self4.java
@@ -1,9 +1,28 @@
 package chapter12.Self4;
 
-public class Chapter12Self4 {
+import javax.swing.*;
+import java.awt.*;
 
+public class Chapter12Self4 extends JFrame {
+    static JLabel simpleLabel=new JLabel("JLabel text");
+    public Chapter12Self4(){
+
+        setTitle("self12-4");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c=getContentPane();
+        c.addMouseListener(new MyMouseListener2());
+
+        c.setLayout(null);
+        simpleLabel.setSize(100,50);
+        simpleLabel.setLocation(110,50);
+        c.add(simpleLabel);
+
+
+        setSize(300,300);
+        setVisible(true);
+    }
 
     public static void main(String[] args){
-
+        new Chapter12Self4();
     }
 }
--- src/chapter12/Self4/MyMouseListener2.java
+++ src/chapter12/Self4/MyMouseListener2.java
@@ -1,4 +1,35 @@
 package chapter12.Self4;
 
-public class MyMouseListener2 {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MyMouseListener2 implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        int x= e.getX();
+        int y=e.getY();
+
+        Chapter12Self4.simpleLabel.setLocation(x,y);
+        Chapter12Self4.simpleLabel.setText(x+", "+y);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }

eb520d14cff5f0515e8518e396ee9918327022d3
DK <dongwook5478@gmail.com>
2023-05-23 14:01:16 +0900

    update 13-2

--- src/chapter13/Program2/Chapter13Program2.java
+++ src/chapter13/Program2/Chapter13Program2.java
@@ -10,6 +10,19 @@ public class Chapter13Program2 extends JFrame {
         c.setLayout(new FlowLayout());
 
         JLabel timerLabel= new JLabel("timerLabel");
+        timerLabel.setFont(new Font("Gothic", Font.ITALIC,80));
+        c.add(timerLabel);
 
+        TimerRunnable runnable=new TimerRunnable(timerLabel);
+        Thread th=new Thread(runnable);
+
+        setSize(300,170);
+        setVisible(true);
+
+        th.start();
+
+    }
+    public static void main(String[] args){
+        new Chapter13Program2();
     }
 }
--- src/chapter13/Program2/TimerRunnable.java
+++ src/chapter13/Program2/TimerRunnable.java
@@ -1,5 +1,25 @@
 package chapter13.Program2;
 
-public class TimerRunnable {
+import javax.swing.*;
 
+public class TimerRunnable implements Runnable{
+    private JLabel timerLabel;
+
+    public TimerRunnable(JLabel timerLabel){
+        this.timerLabel=timerLabel;
+    }
+
+    @Override
+    public void run() {
+        int n=0;
+        while(true){
+            timerLabel.setText(Integer.toString(n));
+            n++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                return;
+            }
+        }
+    }
 }

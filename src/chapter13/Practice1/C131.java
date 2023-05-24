423f1d6645dc8d389ff2d24d769b847eb5135e41
DK <dongwook5478@gmail.com>
2023-05-24 12:27:57 +0900

    update practice 13-2

--- src/chapter13/Practice1/C131.java
+++ src/chapter13/Practice1/C131.java
@@ -7,25 +7,26 @@ import java.awt.*;
  * practice first Thread study
  * @author Dounguk Kim
  * @since 5/20/2023
- * @version v0.0.2
+ * @version v0.0.3
  */
 public class C131 extends JFrame {
-    public C131() {
-        this.setTitle("P131");
+
+    public C131(){
+        this.setTitle("C131");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
+        Container c=getContentPane();
         c.setLayout(new FlowLayout());
 
-        JLabel timerLabel = new JLabel();
-        timerLabel.setFont(new Font("Gothic", Font.ITALIC, 100));
+        JLabel timerLabel=new JLabel();
+        timerLabel.setFont(new Font("Gothic",Font.ITALIC,180));
         c.add(timerLabel);
 
-        TimerThread2 th2 = new TimerThread2(timerLabel);
+        TimerThread2 th=new TimerThread2(timerLabel);
 
         setSize(300,300);
         setVisible(true);
 
-        th2.start();
+        th.start();
     }
     public static void main(String[] args){
         new C131();

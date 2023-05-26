6965055dfdebd8a17d13bc7d1924050301ed2ced
DK <dongwook5478@gmail.com>
2023-05-27 03:00:45 +0900

    update practice 13-3

--- src/chapter13/Practice3/Chapter13Practice3.java
+++ src/chapter13/Practice3/Chapter13Practice3.java
@@ -1,4 +1,29 @@
 package chapter13.Practice3;
 
-public class Chapter13Practice3 {
+import chapter13.Practice2.Chapter13Practice2;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Chapter13Practice3 extends JFrame {
+    public Chapter13Practice3(){
+        setTitle("practice 13-3");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c=new Container();
+        c.setLayout(new FlowLayout());
+
+        FlickeringLabel2 fLabel=new FlickeringLabel2("Hello, ",500);
+        JLabel label=new JLabel("World! ");
+        FlickeringLabel2 fLabel2=new FlickeringLabel2("This is DK" ,300);
+
+        c.add(fLabel);
+        c.add(label);
+        c.add(fLabel2);
+
+        setSize(300,300);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new Chapter13Practice3();
+    }
 }
--- src/chapter13/Practice3/FlickeringLabel2.java
+++ src/chapter13/Practice3/FlickeringLabel2.java
@@ -1,4 +1,36 @@
 package chapter13.Practice3;
 
-public class FlickeringLabel2 {
+import javax.swing.*;
+import java.awt.*;
+
+public class FlickeringLabel2 extends JLabel implements Runnable {
+
+    private long delay;
+    public FlickeringLabel2(String text, long delay){
+        super(text);
+        this.delay=delay;
+        setOpaque(true);
+        Thread th=new Thread(this);
+        th.start();
+    }
+
+    @Override
+    public void run() {
+        int n=0;
+        while(true){
+            if(n==0){
+                setBackground(Color.GREEN);
+            }
+            else {
+                setBackground(Color.BLUE);
+            }
+            if(n==0){n=1;}
+            else{n=0;}
+            try{
+                Thread.sleep(delay);
+            } catch (InterruptedException e){
+                return;
+            }
+        }
+    }
 }

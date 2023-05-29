a44ba566392659c91ade90bb3242bf6f62b460b4
DK <dongwook5478@gmail.com>
2023-05-29 20:32:53 +0900

    add practice 13-5

---
+++ src/chapter13/Practice5/SThread.java
@@ -0,0 +1,23 @@
+package chapter13.Practice5;
+
+import chapter13.Program5.SumThread2;
+
+public class SThread extends Thread{
+    public static long sum;
+    private int start;
+    private int end;
+
+    public SThread(int start,int end){
+        this.start=start;
+        this.end=end;
+    }
+
+    @Override
+    public void run() {
+        long temp=0;
+        for (int i=start;i<=end;i++){
+            temp+=i;
+        }
+        sum+=temp;
+    }
+}

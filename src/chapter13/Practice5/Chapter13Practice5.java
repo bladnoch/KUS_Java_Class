7c52e3785d3fce91e47c8fb39c1117620110a6ac
DK <dongwook5478@gmail.com>
2023-05-29 20:05:37 +0900

    update 13-5

--- src/chapter13/Practice5/Chapter13Practice5.java
+++ src/chapter13/Practice5/Chapter13Practice5.java
@@ -1,4 +1,21 @@
 package chapter13.Practice5;
 
+import chapter13.Practice3.Chapter13Practice3;
+import chapter13.Program5.SumThread;
+
 public class Chapter13Practice5 {
+    public static final int NUM_THREAD=4;
+    public static final int REP_TIMES=2000000000;
+
+    public Chapter13Practice5() throws InterruptedException{
+        long start =System.currentTimeMillis();
+        SumThread.sum=0;
+
+        SumThread[] threads=new SumThread[NUM_THREAD];
+
+    }
+
+    public static void main(String[] args){
+        new Chapter13Practice5();
+    }
 }

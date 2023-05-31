a3faa02027d833b0d4b9447e5901df6141bc3063
DK <dongwook5478@gmail.com>
2023-05-31 12:46:56 +0900

    update 14-2

--- src/chapter14/Program2/Chap14Program2.java
+++ src/chapter14/Program2/Chap14Program2.java
@@ -1,4 +1,45 @@
 package chapter14.Program2;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Chap14Program2 {
+    public static void main(String[] args){
+        Socket socket=null;
+        Scanner sc=new Scanner(System.in);
+
+        try{
+            socket=new Socket("localhost",9999);
+
+            BufferedReader in =new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out =new BufferedWriter((new OutputStreamWriter(socket.getOutputStream())));
+
+            while(true){
+                System.out.println("Client>>");
+                String outputMessage=sc.nextLine();
+
+                if(outputMessage.equalsIgnoreCase("bye")){
+                    out.write(outputMessage+"\n");
+                    out.flush();
+                    break;
+                }
+                out.write(outputMessage+"\n");
+                out.flush();
+                String inputMessage =in.readLine();
+                System.out.println("Server "+inputMessage);
+            }
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                sc.close();
+                if(socket!=null){
+                    socket.close();
+                }
+            } catch(IOException e){
+                System.out.println("An error occurred while communicating with the server.");
+            }
+        }
+    }
 }

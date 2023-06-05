16b23eecefd450c0dd28b38333ce59bc38d54e75
DK <dongwook5478@gmail.com>
2023-06-05 16:16:49 +0900

    add 14-7,8

---
+++ src/Homework_3/Std.java
@@ -0,0 +1,13 @@
+package Homework_3;
+
+public class Std {
+    private String major;
+    private int stdID;
+    private boolean gender;
+    private double height;
+    private double weight;
+
+    public void makeStdID(){
+
+    }
+}

0672b73c77f912e2414725c3fd922e6e7377224a
DK <dongwook5478@gmail.com>
2023-06-08 02:45:32 +0900

    update creat stdID

--- src/Homework_3/Quiz1/Q1.java
+++ src/Homework_3/Quiz1/Q1.java
@@ -1,15 +1,46 @@
 package Homework_3.Quiz1;
 
-public class Q1 {
+public class Q1 extends SimpleStatistics{
     final static int ALLSTD=10000;
+    private static Student std[]=new Student[ALLSTD];
     public static void main(String[] args){
-        Student std[]=new Student[ALLSTD];
+        setStdID();
+    }
+    public static void setStdID(){
+        String zero="0";
+        String temp="";
 
         for (int i=0;i<ALLSTD;i++){
-            if(String.valueOf(i+1).length()<6){
-                String temp;
+            if(String.valueOf(i+1).length()<5){
+                for(int j=0;j<5-String.valueOf(i+1).length();j++){
+                    temp+=zero;
+                }
             }
-            std[i]=new Student(i+1);
+            temp=temp+(i+1);
+            std[i]=new Student(temp);
+            temp="";
+
+            System.out.println(std[i].studentID);
         }
+    }
+
+    @Override
+    public double getMax(double[] variable) {
+        return 0;
+    }
+
+    @Override
+    public double getConditionalMean(String condition, double[] variable) {
+        return 0;
+    }
+
+    @Override
+    public double getMean(double[] variable) {
+        return 0;
+    }
+
+    @Override
+    public double getMin(double[] variable) {
+        return 0;
     }
 }
--- src/Homework_3/Quiz1/Student.java
+++ src/Homework_3/Quiz1/Student.java
@@ -2,7 +2,7 @@ package Homework_3.Quiz1;
 
 public class Student extends Q1{
     private String department;
-    private String studentID;
+    public String studentID;
     private boolean gender;
     private double height;
     private double weight;

a29e77ab27bddd69e5f5cd3273083a563da7e5fa
DK <dongwook5478@gmail.com>
2023-06-15 04:06:15 +0900

    replaced

--- src/Homework_3/Quiz1/Q1.java
+++ src/Homework_3/Quiz1/Q1.java
@@ -47,49 +47,6 @@ public class Q1 extends SimpleStatistics{
         }
     }
 
-    public static void setWidth(){
-        double[] b= new double[ALLSTD];
-        int all=0;
-
-        for (int i=0;i<ALLSTD;i++){
-            b[i]=random.nextGaussian()+68;
-            System.out.println(std[i].getStudentID()+": "+b[i]);
-        }
-        for (int i=0;i<ALLSTD;i++){
-            all+=b[i];
-        }
-        System.out.println("mean weight of all students are "+(all/ALLSTD));
-    }
-    public static void setHeight(){
-        double[] b= new double[ALLSTD];
-        int all=0;
-
-        for (int i=0;i<ALLSTD;i++){
-            b[i]=random.nextGaussian()+173;
-            System.out.println(std[i].getStudentID()+": "+b[i]);
-        }
-        for (int i=0;i<ALLSTD;i++){
-            all+=b[i];
-        }
-        System.out.println("mean height of all students are "+(all/ALLSTD));
-    }
-    public static void setStdID(){
-        String zero="0";
-        String temp="";
-
-        for (int i=0;i<ALLSTD;i++){
-            if(String.valueOf(i+1).length()<5){
-                for(int j=0;j<5-String.valueOf(i+1).length();j++){
-                    temp+=zero;
-                }
-            }
-            temp="Student_"+temp+(i+1);
-//            std[i]=new Student(temp);
-            temp="";
-            System.out.println(std[i].getStudentID());
-        }
-    }
-
 
     public double getMean(double[] variable) {
         double v=0;
@@ -135,5 +92,47 @@ public class Q1 extends SimpleStatistics{
             }
         }
         return temp;
+    }
+    public static void setWidth(){
+        double[] b= new double[ALLSTD];
+        int all=0;
+
+        for (int i=0;i<ALLSTD;i++){
+            b[i]=random.nextGaussian()+68;
+            System.out.println(std[i].getStudentID()+": "+b[i]);
+        }
+        for (int i=0;i<ALLSTD;i++){
+            all+=b[i];
+        }
+        System.out.println("mean weight of all students are "+(all/ALLSTD));
+    }
+    public static void setHeight(){
+        double[] b= new double[ALLSTD];
+        int all=0;
+
+        for (int i=0;i<ALLSTD;i++){
+            b[i]=random.nextGaussian()+173;
+            System.out.println(std[i].getStudentID()+": "+b[i]);
+        }
+        for (int i=0;i<ALLSTD;i++){
+            all+=b[i];
+        }
+        System.out.println("mean height of all students are "+(all/ALLSTD));
+    }
+    public static void setStdID(){
+        String zero="0";
+        String temp="";
+
+        for (int i=0;i<ALLSTD;i++){
+            if(String.valueOf(i+1).length()<5){
+                for(int j=0;j<5-String.valueOf(i+1).length();j++){
+                    temp+=zero;
+                }
+            }
+            temp="Student_"+temp+(i+1);
+//            std[i]=new Student(temp);
+            temp="";
+            System.out.println(std[i].getStudentID());
+        }
     }
 }

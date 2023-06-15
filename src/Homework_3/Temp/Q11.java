63fe26dd0d96d5977a3e27d4a0c9d40338f6b9ab
DK <dongwook5478@gmail.com>
2023-06-16 03:21:32 +0900

    update getMean()

--- src/Homework_3/Temp/Q11.java
+++ src/Homework_3/Temp/Q11.java
@@ -4,28 +4,103 @@ import javax.swing.*;
 
 
 
-public class Q11 {
-    private JFrame frame;
-    public Q11(){
-        frame=new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        frame.setVisible(true);
+public class Q11 extends SimpleStatistics {
+    final static int STDNUM=10;
+    private static int males=0;
+    private static int females=0;
+    private static Students[] temp=new Students[STDNUM];
+    private static double[] tempH=new double[STDNUM];
+    private static double[] tempW=new double[STDNUM];
 
+    /**
+     * constructor of JFrame
+      */
+//    private JFrame frame;
+//    public Q11(){
+//        frame=new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(300,300);
+//        frame.setVisible(true);
+//    }
+
+    /**
+     * setting up the variables for abstract methods
+     */
+    public static void genCount(){
+        for(int i=0;i<STDNUM;i++){
+            if (temp[i].isMale()) {
+                males++;
+            } else {
+                females++;
+            }
+            tempH[i]=temp[i].getHeight();
+            tempW[i]=temp[i].getWeight();
+        }
+        System.out.println("num of male : "+males+"\nnum of female : "+females);
     }
 
+
     public static void main(String[] args){
-        Students[] temp=new Students[10];
+
+        Q11 in=new Q11();
         for(int i=0;i<10;i++){
             temp[i]=new Students();
             System.out.println(temp[i].getStudentID()+" : "+(temp[i].isMale()? "male":"female"));
             System.out.println("height: "+temp[i].getHeight()+"\nweight: "+temp[i].getWeight());
-            System.out.println(" ");
+
         }
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new Q11();
-            }
-        });
+        genCount(); //setting up all variables of the abstract methos's parameter
+        System.out.println("\n get mean of height, and weight : "+in.getMean(tempH)+", "+in.getMean(tempW));
+
+
+        /**
+         * to running Jframe
+         */
+//        SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                new Q11();
+//            }
+//        });
+    }
+
+    /**
+     * @param variable
+     * @return
+     */
+    @Override
+    public double getMean(double[] variable) {
+        double forReturn=0;
+        for (int i=0;i<STDNUM;i++){
+            forReturn+=variable[i];
+        }
+        return forReturn/STDNUM;
+    }
+
+    /**
+     * @param condition
+     * @param variable
+     * @return
+     */
+    @Override
+    public double getConditionalMean(String condition, double[] variable) {
+        return 0;
+    }
+
+    /**
+     * @param variable
+     * @return
+     */
+    @Override
+    public double getMax(double[] variable) {
+        return 0;
+    }
+
+    /**
+     * @param variable
+     * @return
+     */
+    @Override
+    public double getMin(double[] variable) {
+        return 0;
     }
 }

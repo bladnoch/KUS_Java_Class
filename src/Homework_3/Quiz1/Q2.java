314e4dfc4a05eb5a359ce33ed2c8e52762f5e6a7
DK <dongwook5478@gmail.com>
2023-06-16 08:37:41 +0900

    update Q2

--- src/Homework_3/Quiz1/Q2.java
+++ src/Homework_3/Quiz1/Q2.java
@@ -1,7 +1,40 @@
 package Homework_3.Quiz1;
 
-public class Q2 extends Q1{
+import javax.swing.*;
+import java.awt.*;
+
+public class Q2{
+    final static int APROX=10000;
+
+
+    protected JFrame frame;
+    protected JTextField genderField;
+    protected JTextField weightField;
+    protected JTextField gradeField;
+    public Q2(){
+        frame = new JFrame("add new student");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new GridLayout(4,1));
+
+        frame.add(new JLabel("Gender (male/female):"));
+        genderField = new JTextField(12);
+        frame.add(genderField);
+
+        frame.add(new JLabel("Weight (kg):"));
+        weightField = new JTextField(12);
+        frame.add(weightField);
+
+        frame.add(new JLabel("Grade (0~4.5):"));
+        gradeField = new JTextField(12);
+        frame.add(gradeField);
+
+        frame.setVisible(true);
+    }
     public static void main(String[] args){
+        Q2 in=new Q2();
+//        Q1 temp=new Q1();
 
     }
 }

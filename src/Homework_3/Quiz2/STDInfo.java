4bd0d44f469f2c6567c5996d9a50bc4a542eb1fc
DK <dongwook5478@gmail.com>
2023-06-16 08:46:09 +0900

    add variables in STDInfo

--- src/Homework_3/Quiz2/STDInfo.java
+++ src/Homework_3/Quiz2/STDInfo.java
@@ -1,4 +1,22 @@
 package Homework_3.Quiz2;
 
+import java.util.Random;
+
 public class STDInfo {
+    private boolean gender;
+    private double weight;
+    private double grade;
+    private Random rd=new Random();
+
+    STDInfo(){
+        this.gender=rd.nextBoolean();
+        this.weight=1.0;
+        this.grade=0.0;
+    }
+    STDInfo(boolean gender,double weight,double grade){
+        this.gender=gender;
+        this.weight=weight;
+        this.grade=grade;
+    }
+
 }

4ff5757a29a207275a8a3ebd67632225a26127ea
DK <dongwook5478@gmail.com>
2023-06-16 02:05:44 +0900

    add constructor, std id, etc.

--- src/Homework_3/Temp/Q11.java
+++ src/Homework_3/Temp/Q11.java
@@ -2,6 +2,11 @@ package Homework_3.Temp;
 
 public class Q11 {
     public static void main(String[] args){
+        Students[] temp=new Students[10];
+        for(int i=0;i<10;i++){
+            temp[i]=new Students();
+            System.out.println(temp[i].getStudentID()+"\n"+temp[i].isMale());
 
+        }
     }
 }
--- src/Homework_3/Temp/Students.java
+++ src/Homework_3/Temp/Students.java
@@ -1,4 +1,48 @@
 package Homework_3.Temp;
 
-public class Students {
+import java.util.Random;
+
+public class Students{
+    private String department;
+    private String studentID;
+    private boolean isMale;
+    private double height;
+    private double weight;
+    private static int count=1;
+    private static Random rd=new Random();
+
+    public Students() {
+        this("undecided", "studentID",rd.nextBoolean() , 0.0, 0.0);
+    }
+
+    public Students(String studentID, boolean isMale, double height) {
+        this("undecided", studentID, isMale, height, 0.0);
+    }
+
+    public Students(String department, String studentID, boolean isMale, double height, double weight) {
+        this.department = department;
+        this.studentID = String.format("Student_%05d", count++);
+        this.isMale = isMale;
+        this.height = height;
+        this.weight = weight;
+    }
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }

08f91345d1c8e5b884a633d11a01d3bdbd9262a3
DK <dongwook5478@gmail.com>
2023-06-16 02:49:22 +0900

    add jframe

--- src/Homework_3/Temp/Q11.java
+++ src/Homework_3/Temp/Q11.java
@@ -1,6 +1,19 @@
 package Homework_3.Temp;
 
+import javax.swing.*;
+
+
+
 public class Q11 {
+    private JFrame frame;
+    public Q11(){
+        frame=new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+
+    }
+
     public static void main(String[] args){
         Students[] temp=new Students[10];
         for(int i=0;i<10;i++){
@@ -9,5 +22,10 @@ public class Q11 {
             System.out.println("height: "+temp[i].getHeight()+"\nweight: "+temp[i].getWeight());
             System.out.println(" ");
         }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Q11();
+            }
+        });
     }
 }

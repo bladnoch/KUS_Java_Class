6ca5bb939495d422d8aa57d975e73d144ba1e7c6
DK <dongwook5478@gmail.com>
2023-06-16 09:45:46 +0900

    save before big change

--- src/Homework_3/Quiz2/Students.java
+++ src/Homework_3/Quiz2/Students.java
@@ -2,18 +2,18 @@ package Homework_3.Quiz2;
 
 import java.util.Random;
 
-public class STDInfo {
+public class Students {
     private boolean gender;
     private double weight;
     private double grade;
     private Random rd=new Random();
 
-    STDInfo(){
+    Students(){
         this.gender=rd.nextBoolean();
         this.weight=1.0;
         this.grade=0.0;
     }
-    STDInfo(boolean gender,double weight,double grade){
+    Students(boolean gender, double weight, double grade){
         this.gender=gender;
         this.weight=weight;
         this.grade=grade;
